30bf92cb442fe3bb6d0080ef098c0b7ab3df2dad
zaiky09 <zamilmozamil@outlook.com>
2023-02-02 12:48:28 +0300

    Used interpolation and slice method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+
+const currentUser = "Grace Hopper" ;
+
+//let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+//Above is an example of concatenation
+
+//Below is using interpolation
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//.toUpperCase used to change sentence case
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//let shortGreeting = "Welcome, ";
+
+//Below we use .slice() to get first initial of Grace
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
